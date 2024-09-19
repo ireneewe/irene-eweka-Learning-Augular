5702f8f7a31bfde636cf8b851bfcaf4bda1d1498
Irene Eweka <w0780653@myscc.ca>
2024-09-18 20:42:22 -0400

    added new data type with at least five properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +12,7 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title : string = 'Irene-angular learning';
   fullName : string ='Irene Eweka';
+
+  //here you make your hair objects and put them in an array
+
 }
---
+++ src/app/shared/models/hair.ts
@@ -0,0 +1,9 @@
+export interface Hair {
+  //What describes hair
+  id:number,
+  hairName:String,
+  hairType: String,
+  hairTexture:String,
+  color?: String
+
+}
