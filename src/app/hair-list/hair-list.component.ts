738342ef09ab779050a19bb80f27d169053348e0
Irene Eweka <w0780653@myscc.ca>
2024-09-26 15:22:44 -0400

    question 12 compeleted, odd and even

--- src/app/hair-list/hair-list.component.ts
+++ src/app/hair-list/hair-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {hair} from "../shared/models/hair";
 import {FormsModule} from "@angular/forms";
+import {HairListItemComponent} from "../hair-list-item/hair-list-item.component";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-hair-list',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    HairListItemComponent,
+    NgForOf,
+    NgClass
   ],
   templateUrl: './hair-list.component.html',
   styleUrl: './hair-list.component.css'
