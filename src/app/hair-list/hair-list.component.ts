c51c8e28047d130a6fff5a38ab0d535b65d6594f
Irene Eweka <w0780653@myscc.ca>
2024-09-26 19:19:33 -0400

    created the service

--- src/app/hair-list/hair-list.component.ts
+++ src/app/hair-list/hair-list.component.ts
@@ -18,12 +18,6 @@ import {NgClass, NgForOf} from "@angular/common";
 })
 export class HairListComponent {
 
-  hairList: hair[] = [
-    { id: 1, hairName: "box braid", hairType: "small", hairTexture: "kinky", color: false },
-    { id: 2, hairName: "leave out", hairType: "flat iron", hairTexture: "natural", color: true },
-    { id: 3, hairName: "foc lox", hairType: "middle", hairTexture: "plastic", color: false },
-    { id: 4, hairName: "wig", hairType: "side part", hairTexture: "soft", color: true },
-  ];
 
 
 
---
+++ src/app/shared/mockData-hair.ts
@@ -0,0 +1,8 @@
+import {hair} from "./models/hair";
+
+export const hairList: hair[] = [
+  { id: 1, hairName: "box braid", hairType: "small", hairTexture: "kinky", color: false },
+  { id: 2, hairName: "leave out", hairType: "flat iron", hairTexture: "natural", color: true },
+  { id: 3, hairName: "foc lox", hairType: "middle", hairTexture: "plastic", color: false },
+  { id: 4, hairName: "wig", hairType: "side part", hairTexture: "soft", color: true },
+];
