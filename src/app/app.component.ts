6eb1a2b10c0fb7c03289cca29f06522786d840bc
Irene Eweka <w0780653@myscc.ca>
2024-10-03 18:36:55 -0400

    assignment 4 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {hair} from "./shared/models/hair";
 import {NgForOf, NgIf} from "@angular/common";
 import {HairListComponent} from "./hair-list/hair-list.component";
-
+import {HairListItemComponent} from "./hair-list-item/hair-list-item.component";
+import {hairList} from "./shared/mockData-hair";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, HairListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, HairListComponent, HairListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +17,9 @@ import {HairListComponent} from "./hair-list/hair-list.component";
 export class AppComponent {
   //here you make your hair objects and put them in an array
   title = 'Irene Hair Store';
+
+
+  protected readonly hairList = hairList;
 }
 
 
--- src/app/hair-list/hair-list.component.ts
+++ src/app/hair-list/hair-list.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {hair} from "../shared/models/hair";
 import {FormsModule} from "@angular/forms";
 import {HairListItemComponent} from "../hair-list-item/hair-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
 import {hairList} from "../shared/mockData-hair";
+import {HairService} from "../services/hair.service";
 
 @Component({
   selector: 'app-hair-list',
@@ -17,8 +18,23 @@ import {hairList} from "../shared/mockData-hair";
   templateUrl: './hair-list.component.html',
   styleUrl: './hair-list.component.css'
 })
-export class HairListComponent {
+export class HairListComponent implements OnInit{
 
+  displayColomus:String[]=['id', 'name', 'texture', 'type'];
+  hairList: hair[] =[];
 
-  protected readonly hairList = hairList;
+  constructor(private hairService: HairService) {
+  }
+  ngOnInit() {
+    this.hairService.getHair().subscribe({
+      next: (data: hair[]) => this.hairList = data,
+      error:err => console.error("error finding hair", err),
+      complete:() => console.log("finding completed ")
+    })
+  }
+  selectedHairItem?: hair;
+  selectedHair(hair: hair): void{
+    this.selectedHairItem = hair;
+  }
+
 }
