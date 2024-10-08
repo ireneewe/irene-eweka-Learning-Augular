d8af7a25887b0eefe7c1263183842cba6cc57572
Irene Eweka <w0780653@myscc.ca>
2024-10-08 19:30:59 -0400

    list of routes defined

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {HairListComponent} from "./app/hair-list/hair-list.component";
+import {HairListItemComponent} from "./app/hair-list-item/hair-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
+
+const routes: Routes =[
+  {path: 'hair', component:HairListComponent},
+  {path: 'hair/:id', component: HairListItemComponent}
+]
+
+bootstrapApplication(AppComponent, {
+ providers:[provideRouter(routes)]
+})
   .catch((err) => console.error(err));
