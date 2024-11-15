4a5e9639b2da0280ac0940f000c91abb61fb285e
Irene Eweka <w0780653@myscc.ca>
2024-11-14 19:17:00 -0500

    assignment 8 completed

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
 import {HairListComponent} from "./app/hair-list/hair-list.component";
-import {HairListItemComponent} from "./app/hair-list-item/hair-list-item.component";
-import {ModifyHairComponent} from "./app/Modify-hair/modify-hair.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {importProvidersFrom} from "@angular/core";
 
 
 const routes: Routes =[
   {path:'', redirectTo: '/hair', pathMatch: 'full'},
   {path: 'hair', component:HairListComponent},
-  {path: 'hair/:id', component: HairListItemComponent},
-  {path: 'modify-hair', component: ModifyHairComponent},
-  {path:"**", component:PageNotFoundComponent} //error route
-]
+  {path:  'hair/:id',
+      loadComponent: () =>
+        import('./app/hair-list-item/hair-list-item.component').then(m => m.HairListItemComponent) }, //Lazy Loaded
+  { path: 'modify-hair',
+    loadComponent: () =>
+      import('./app/Modify-hair/modify-hair.component').then(m => m.ModifyHairComponent) },
+  { path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
+];
 
 bootstrapApplication(AppComponent, {
- providers:[provideRouter(routes)]
+ providers:[
+   provideRouter(routes),
+ importProvidersFrom()]
 })
   .catch((err) => console.error(err));
